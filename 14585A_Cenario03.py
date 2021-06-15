e9b591cb638ca558e8f54de8481b3549a0d65c8a
tluan <tluancm@gmail.com>
2021-06-15 15:57:44 -0300

    cenario3 calculo media

--- 14585A_Cenario03.py
+++ 14585A_Cenario03.py
@@ -20,33 +20,48 @@ def saveAstxt():#gera txt com resultados
     os.chdir(r'C:\Users\VNTTAMA\Desktop\Relatorios\txt')
     sys.stdout = open(f"{file_out}.txt", 'wt')
     print('Corrente média: ' + str(media) + ' A\n'+'Corrente minima: ' + str(minimo)+' A\n'+'Corrente maxima: ' + str(maximo)+' A')
+    for k in range(0, 5):
+        print(f'ínicio: {ti[k]}       fim: {tf[k]}            duração: {dt[k]}       corrente media: {mean[k]}   mA ')
     sys.stdout.close()
+
 
 
 file_in = input("Digite o nome do arquivo de entrada: ")
 file_out = input("Digite o nome do arquivo de saída: ")
 
-model = (input('Este modelo tem impressao?[s][n] '))
+#model = (input('Este modelo tem impressao?[s][n] '))
+ti =[]
+tf = []
+dt = []
 for k in range(0,5):
     ti.append(int(input(f'ti_{k} = ')))
     tf.append(int(input(f'tf_{k} = ')))
     dt.append(tf[k] - ti[k])
-    
+
+ti_m = [c*2 for c in ti]    
+tf_m = [c*2 for c in tf]
 
 
 df = pd.read_csv(file_in,skiprows=(3))#converte o arquivo csv em dataframe
-df2 = df.rename(columns = {'Active Instrument A Channel 1 Current Avg': "Corrente"}, inplace= False)#renomeia coluna de df e salva em df2
-df2['Time'] = int(round(df2['Time']))
+df['Time'] = df['Time'].round(decimals = 1)
+df2 = df.rename(columns = {'Active Instrument A Channel 3 Current Avg': "Corrente"}, inplace= False)#renomeia coluna de df e salva em df2
 df3 = df2.set_index('Time')#troca a indexacao para a coluna Time e salva em df3
 df3["Corrente"] = df3["Corrente"].multiply(1000)#multiplica toda coluna corrente por 1000 para mostrar no grafico em mA
 df4 = pd.DataFrame(df3, columns = ["Corrente"])#a partir do df3 cria um df4 com apenas colunas nomeadas correntes
+print(df4)
+mean = []
+for k in range(0, 5):
+    mean.append(df4.iloc[ti_m[k]:tf_m[k]+1,0].mean())
+# maxi = []
+# for k in range(0, 5):
+#     maxi.append(df4.iloc[ti[k]:tf[k]+1,0].max())    
 
 print("-"*50)
 print(f"{file_out}.png foi salvo em Relatorios\imagens\n{file_out}.pdf foi salvo em Relatorios\pdf\n{file_out}.txt foi salvo em Relatorios"+'\\txt')
 print("-"*25+"FIM"+"-"*25)
 
 df4.plot(xlabel= 'tempo[s]', ylabel = 'Corrente[mA]', grid = True, legend = False, title = f'{file_out}', figsize = (19.20,10.80))
-plt.savefig(f'Resultados\imagens\{file_out}.jpg', dpi = 600)
+plt.savefig(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg', dpi = 600)
 
 df4["Corrente"] = df4["Corrente"].multiply(1/1000)#divide a corrente por 1000 para mostrar resultados  em A
 media = (round(df4["Corrente"].mean(), 5))
