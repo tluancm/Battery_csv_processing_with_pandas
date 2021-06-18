7987006a7ada6d0a42eda42f4d398ec4f6c88a8a
tluan <tluancm@gmail.com>
2021-06-18 15:16:28 -0300

    Versao funcional de todos cenarios 1.00

--- 14585A_Cenario03.py
+++ 14585A_Cenario03.py
@@ -1,16 +1,15 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from fpdf import FPDF
-import sys
-import os
-import statistics as sts
-import time
+import pandas as pd#provide way to process a csv file
+import matplotlib.pyplot as plt#provide way to generate graph
+from fpdf import FPDF# print output as pdf file
+import sys#provide access to variable wich interact with the phyon interpreter
+import os# provid way to interact with the OS
+import statistics as sts#can operate easily on lists
 
-def saveAsPDF():#gera pdf com resultados e imagem
+def saveAsPDF():#print output as pdf file
     pdf = FPDF()
     pdf.add_page()
-    epw = pdf.w -2*pdf.l_margin
-    col_widht = epw/5 -3
+    epw = pdf.w -2*pdf.l_margin#widht of page minus 2x the margin, make possible to create cells with wide from margin to margin
+    col_widht = epw/5 -3#adjusting the columns width size to fit on page
     col_widht2 = epw/3
     pdf.set_font('Times', 'B', 16)
     pdf.cell(w = 0, h=20, txt = f'Resultados {file_out}', align = 'C', ln=1 )
@@ -22,7 +21,8 @@ def saveAsPDF():#gera pdf com resultados e imagem
     pdf.cell(w = col_widht, h =6, border =1, align = 'C',  txt = "Duração [s]")
     pdf.cell(w = col_widht+12, h =6, border =1,ln =1, align = 'C', txt = "Corrente média [mA]")
     for k in range(0,5):
-        pdf.cell(w = col_widht, h =6, border = 1, align = 'C',  txt = f"{k}" )
+        i = k+1
+        pdf.cell(w = col_widht, h =6, border = 1, align = 'C',  txt = f"{i}" )
         pdf.cell(w = col_widht, h =6, border =1, align = 'C',  txt = f"{ti[k]}")
         pdf.cell(w = col_widht, h =6, border =1, align = 'C',  txt = f"{tf[k]}")
         pdf.cell(w = col_widht, h =6, border =1, align = 'C', txt = f"{dt[k]}")
@@ -37,37 +37,32 @@ def saveAsPDF():#gera pdf com resultados e imagem
     pdf.cell(w = col_widht2,h= 6, border= 1,align= 'C', txt= "Corrente 1ª via [mA]" )
     pdf.cell(w = col_widht2,h= 6, border= 1, ln=1, align= 'C', txt= "Corrente 2ª via [mA]" )
     for k in range(0,5):
-        pdf.cell(w = col_widht2, h =6, border = 1, align = 'C',  txt = f"{k}" )
+        i = k+1
+        pdf.cell(w = col_widht2, h =6, border = 1, align = 'C',  txt = f"{i}" )
         pdf.cell(w = col_widht2, h =6, border =1, align = 'C',  txt = f"{imp1[k]}")
         pdf.cell(w = col_widht2, h =6, border =1,ln= 1,  align = 'C',  txt = f"{imp2[k]}")
     pdf.cell(w = col_widht2, h= 6, border = 1, align= 'C', txt= 'Média')    
     pdf.cell(w = col_widht2, h= 6,border= 1, align= 'C', txt= f'{mean_imp1}' )
     pdf.cell(w = col_widht2, h= 6,border= 1, align= 'C', txt= f'{mean_imp2}' )
-    pdf.image(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg',x = 0, y = 125, w =200, h = 150)
+    pdf.image(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg',x = -10, y = 125, w =225, h = 150)
     pdf.output(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\pdf\\{file_out}.pdf','F')
 
-
-def saveAstxt():#gera txt com resultados
-    os.chdir(r'C:\Users\VNTTAMA\Desktop\Relatorios\txt')
-    sys.stdout = open(f"{file_out}.txt", 'wt')
-    print('Corrente média: ' + str(media) + ' A\n'+'Corrente minima: ' + str(minimo)+' A\n'+'Corrente maxima: ' + str(maximo)+' A')
-    for k in range(0, 5):
-        print(f'ínicio: {ti[k]}       fim: {tf[k]}            duração: {dt[k]}       corrente media{k}: {mean2[k]}   mA ')
-    print(f'corrente media nas transações: {mean_t}   mA')    
+def saveAstxt():#open a txt file and write the results
+    os.chdir(r'C:\Users\VNTTAMA\Desktop\Relatorios\txt')#create directory
+    sys.stdout = open(f"{file_out}.txt", 'wt')#open file to write
+    print(text)
     sys.stdout.close()
-
-
 
 file_in = input("Digite o nome do arquivo de entrada: ")
 file_out = input("Digite o nome do arquivo de saída: ")
+model = (input('Este modelo tem impressao?[s] ou [n]:  '))
 
-model = (input('Este modelo tem impressao?[s][n]:  '))
 ti =[]
 tf = []
 dt = []
 imp1 = []
 imp2 = []
-for k in range(0,5):
+for k in range(0,5):#read as input the time, end and difference between transaction times; read the currente on terminal print
     ti.append(int(input(f'ti_{k} = ')))
     tf.append(int(input(f'tf_{k} = ')))
     if (model == 's'or model == 'S'):
@@ -77,44 +72,54 @@ for k in range(0,5):
         imp1.append('-')
         imp2.append('-')
     dt.append(tf[k] - ti[k])
-if (model == 's' or model == 'S'):    
+if (model == 's' or model == 'S'):#Calculate the terminal print mean
     mean_imp1 = round(sts.mean(imp1), 3)
     mean_imp2 = round(sts.mean(imp2), 3) 
 else:
-    mean_imp1 = '-'
+    mean_imp1 = '-'#if not a printer terminal print "-" on pdf
     mean_imp2 = '-'    
-dt_mean = sts.mean(dt)
-ti_m = [c*2 for c in ti]    
+dt_mean = sts.mean(dt)#calculate the mean of transaction time
+ti_m = [c*2 for c in ti]#convert the time inputs to represent your associated index   
 tf_m = [c*2 for c in tf]
 
-
-df = pd.read_csv(file_in,skiprows=(3))#converte o arquivo csv em dataframe
+df = pd.read_csv(file_in,skiprows=(3))#convert csv to dataframe
 df['Time'] = df['Time'].round(decimals = 1)
-df2 = df.rename(columns = {'Active Instrument A Channel 3 Current Avg': "Corrente"}, inplace= False)#renomeia coluna de df e salva em df2
-df3 = df2.set_index('Time')#troca a indexacao para a coluna Time e salva em df3
-df3["Corrente"] = df3["Corrente"].multiply(1000)#multiplica toda coluna corrente por 1000 para mostrar no grafico em mA
-df4 = pd.DataFrame(df3, columns = ["Corrente"])#a partir do df3 cria um df4 com apenas colunas nomeadas correntes
+df2 = df.rename(columns = {'Active Instrument A Channel 3 Current Avg': "Corrente"}, inplace= False)#rename the column "Corrente"
+df3 = df2.set_index('Time')#create df3 with index seted as "Time" column from df2
+df3["Corrente"] = df3["Corrente"].multiply(1000)
+df4 = pd.DataFrame(df3, columns = ["Corrente"])#create a subdataframe df4 with column "Corrente" from df3
 
 mean = []
 
-for k in range(0, 5):
+for k in range(0, 5):#localize the transaction times based on time index
     mean.append(df4.iloc[ti_m[k]:tf_m[k]+1,0].mean())
-mean2 = [round(n, 3) for n in mean ]  
-mean_t = round(sts.mean(mean), 3)
+mean2 = [round(n, 3) for n in mean ] #create a list with rounded values from variable mean
+mean_t = round(sts.mean(mean), 3)#create a variable with the transaction times mean
 
-print("-"*50)
-print(f"{file_out}.png foi salvo em Relatorios\imagens\n{file_out}.pdf foi salvo em Relatorios\pdf\n{file_out}.txt foi salvo em Relatorios"+'\\txt')
-print("-"*25+"FIM"+"-"*25)
-
-df4.plot(xlabel= 'tempo[s]', ylabel = 'Corrente[mA]', grid = True, legend = False, title = f'{file_out}', figsize = (19.20,10.80))
+df4.plot( grid = True, legend = False, figsize = (19.20,10.80))
+plt.xlabel('tempo [s]', fontsize=22)
+plt.ylabel('Corrente [mA]', fontsize=22)
+plt.suptitle(f'{file_out}'+" gráfico", fontsize= 26)
 plt.savefig(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg', dpi = 600)
-#time.sleep(4)
 
-
-df4["Corrente"] = df4["Corrente"].multiply(1/1000)#divide a corrente por 1000 para mostrar resultados  em A
+df4["Corrente"] = df4["Corrente"].multiply(1/1000)#variables will be presented as Ampere unity
 media = (round(df4["Corrente"].mean(), 5))
 minimo = (round(df4["Corrente"].min(), 5))
 maximo = (round(df4["Corrente"].max(), 5))
 
 saveAsPDF()
-#saveAstxt()
+
+print("-"*50)
+print(f"{file_out}.png foi salvo em Relatorios\imagens\n{file_out}.pdf foi salvo em Relatorios\pdf\n{file_out}.txt foi salvo em Relatorios"+'\\txt')
+print("-"*25+"FIM"+"-"*25)
+
+text = {'Corrente média': media, 'Corrente minima': minimo, 'Corrente máxima': maximo, 
+'Duração transação 1':dt[0], 'Duração transação 2':dt[1], 'Duração transação 3':dt[2], 'Duração transação 4':dt[3], 'Duração transação 5':dt[4],
+'Corrente media transação 1': mean2[0], 'Corrente media transação 2': mean2[1],'Corrente media transação 3': mean2[2],
+'Corrente media transação 4': mean2[3],'Corrente media transação 5': mean2[4], 'Média das correntes de transação': mean_t, 
+'Corrente 1ª via #1': imp1[0], 'Corrente 1ª via #2': imp1[1], 'Corrente 1ª via #3': imp1[2], 'Corrente 1ª via #4': imp1[3],
+'Corrente 1ª via #5': imp1[4], 'Média da corrente de 1ª via': mean_imp1, 'Corrente 2ª via 1': imp2[0], 'Corrente 2ª via #2': imp2[1],  
+'Corrente 2ª via #3': imp2[2],'Corrente 2ª via #4': imp2[3],  'Corrente 2ª via #5': imp2[4], 'Média da corrente de 2ª via': mean_imp2
+}
+
+saveAstxt()
--- 14585A_Cenario05.py
+++ 14585A_Cenario05.py
@@ -1,23 +1,23 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-from fpdf import FPDF
-import sys
-import os
-import time
+import pandas as pd#provide way to process a csv file
+import matplotlib.pyplot as plt#provide way to generate graph
+from fpdf import FPDF# print output as pdf file
+import sys#provide access to variable wich interact with the phyon interpreter
+import os# provid way to interact with the OS
+import time#can read times input as hh:mm:ss
 
-def saveAsPDF():#gera pdf com resultados e imagem
+def saveAsPDF():#print output as pdf file
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font('Times', 'B', 16)
     pdf.cell(w = 0, h=20, txt = f'Resultados {file_out}', align = 'C', ln=1 )
     pdf.set_font('Times', 'B', 12)
-    epw = pdf.w -2*pdf.l_margin
-    cold_widht = epw/4
+    epw = pdf.w -2*pdf.l_margin#widht of page minus 2x the margin, make possible to create cells with wide from margin to margin
+    cold_widht = epw/4#adjusting the columns width size to fit on page
 
     pdf.cell(w= cold_widht-6, h= 6, align = 'C',border =1, txt= 'Tensão média [V]' )
     pdf.cell(w= cold_widht-6, h= 6, align = 'C',border= 1, txt= 'Tensão miníma [V]')
     pdf.cell(w= cold_widht-6, h= 6, align = 'C',border= 1,txt= 'Tensão máxima [V]' )
-    pdf.cell(w= cold_widht+18, h= 6,align = 'C', border= 1, ln=1,txt= 'Tempo de carregamento [h:m:s]' )
+    pdf.cell(w= cold_widht+18, h= 6,align = 'C', border= 1, ln=1,txt= 'Tempo de carregamento [hh:mm:ss]' )
 
     pdf.cell(w= cold_widht-6, h= 6, align = 'C',border= 1,txt= f'{media}' )
     pdf.cell(w= cold_widht-6, h= 6, align = 'C',border= 1,txt= f'{minimo}')
@@ -25,89 +25,96 @@ def saveAsPDF():#gera pdf com resultados e imagem
     pdf.cell(w= cold_widht+18, h= 6,align = 'C', border= 1, ln=1,txt= f'{t_descarga}' )
 
     pdf.cell(w = 0, h= 6,border= 0,align = 'C', ln= 1, txt='' )
-    cold_widht2 = epw/3
-    cold_widht3 = epw/4
-    pdf.cell(w= cold_widht3, h= 6,align = 'C', border= 1,txt= ' ' )
-    pdf.cell(w= 2*cold_widht3, h= 6,align = 'C', border= 1,txt= 'Tempo' )
-    pdf.cell(w= cold_widht3, h= 6,align = 'C',ln=1, border= 1,txt= ' ' )
+
+    col_widht2 = epw/4
+    pdf.cell(w= col_widht2, h= 6,align = 'C', border= 1,txt= ' ' )
+    pdf.cell(w= 2*col_widht2, h= 6,align = 'C', border= 1,txt= 'Tempo' )
+    pdf.cell(w= col_widht2, h= 6,align = 'C',ln=1, border= 1,txt= ' ' )
     
-    pdf.cell(w= cold_widht3, h= 6,align = 'C', border= 1,txt= 'Barras #' )
-    pdf.cell(w= cold_widht3, h= 6,align = 'C', border= 1,txt= 's' )
-    pdf.cell(w= cold_widht3, h= 6,align = 'C', border= 1,txt= 'h:m:s' )
-    pdf.cell(w= cold_widht3, h= 6,align = 'C',ln= 1, border= 1,txt= 'Bateria[%]')
+    pdf.cell(w= col_widht2, h= 6,align = 'C', border= 1,txt= 'Barras #' )
+    pdf.cell(w= col_widht2, h= 6,align = 'C', border= 1,txt= 's' )
+    pdf.cell(w= col_widht2, h= 6,align = 'C', border= 1,txt= 'hh:mm:ss' )
+    pdf.cell(w= col_widht2, h= 6,align = 'C',ln= 1, border= 1,txt= 'Bateria[%]')
     
     for k in range(0, barras):
-        pdf.cell(w = cold_widht3, h= 6,align = 'C', border=1, txt= f'{k}' )
-        pdf.cell(w = cold_widht3, h=6,align = 'C', border=1, txt= f'{t_conv_list[k]}')
-        pdf.cell(w = cold_widht3, h= 6,align = 'C', border=1, txt= f'{t_header[k]}')
-        pdf.cell(w = cold_widht3, h=6,align = 'C', border= 1, ln=1, txt= f'bbbbb')
+        temp = barras -1 -k
+        pdf.cell(w = col_widht2, h= 6,align = 'C', border=1, txt= f'{temp}' )
+        pdf.cell(w = col_widht2, h=6,align = 'C', border=1, txt= f'{t_conv_list[k]}')
+        pdf.cell(w = col_widht2, h= 6,align = 'C', border=1, txt= f'{t_header[k]}')
+        pdf.cell(w = col_widht2, h=6,align = 'C', border= 1, ln=1, txt= f'{battery[k]}')
 
-    pdf.image(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg',x = 0, y = 100, w =200, h = 150)
+    pdf.image(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg',x = -10, y = 100, w =225, h = 150)
     pdf.output(f"C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\pdf\\{file_out}.pdf",'F')
 
-
-def saveAstxt():#gera txt com resultados
-    os.chdir(r'C:\Users\VNTTAMA\Desktop\Relatorios\txt')
-    sys.stdout = open(f"{file_out}.txt", 'wt')
-    print('Tensão média: ' + str(media) + ' V\n'+'Tensão minima: ' + str(minimo)+' V\n'+'Tensão maxima: ' + str(maximo)+' V\n'+'Tempo de descarregamento: '+t_descarga)
+def saveAstxt():#open a txt file and write the results
+    os.chdir(r'C:\Users\VNTTAMA\Desktop\Relatorios\txt')#create directory
+    sys.stdout = open(f"{file_out}.txt", 'wt')#open file to write
+    print(text)
     sys.stdout.close()
-
 
 file_in = input("Digite o nome do arquivo de entrada: ")
 file_out = input("Digite o nome do arquivo de saída: ")
 
+df = pd.read_csv(file_in,skiprows=(3))#convert csv to dataframe
+df2 = df.rename(columns = {'File1 Instrument A Channel 1 Voltage Avg': "Tensão"}, inplace= False)#create df2 with df renamed column
+df3 = df2.set_index('Time')#create df3 with index as df2 column
+df4 = pd.DataFrame(df3, columns = ["Tensão"])#create a subdf df4 with column "Tensão" from df3
 
-df = pd.read_csv(file_in,skiprows=(3))#converte o arquivo csv em dataframe
-df2 = df.rename(columns = {'File1 Instrument A Channel 1 Voltage Avg': "Tensão"}, inplace= False)#renomeia coluna de df e salva em df2
-df3 = df2.set_index('Time')#troca a indexacao para a coluna Time e salva em df3
-df4 = pd.DataFrame(df3, columns = ["Tensão"])#a partir do df3 cria um df4 com apenas colunas nomeadas Tensãos
-print("-"*50)
-print(f"{file_out}.png foi salvo em Relatorios\imagens\n{file_out}.pdf foi salvo em Relatorios\pdf\n{file_out}.txt foi salvo em Relatorios"+'\\txt')
-print("-"*25+"FIM"+"-"*25)
+df4['Bateria[%]'] = (df4["Tensão"]-df4["Tensão"].min())*100/(df4["Tensão"].max()-df4["Tensão"].min())#Calculate battery %
+df5 = pd.DataFrame(df4, columns = ['Bateria[%]'])#create a df5 dataframe with df4 column
 
-df4['Bateria[%]'] = (df4["Tensão"]-df4["Tensão"].min())*100/(df4["Tensão"].max()-df4["Tensão"].min())
-df5 = pd.DataFrame(df4, columns = ['Bateria[%]'])
-df5.plot(xlabel= 'tempo[s]', ylabel = 'Bateria[%]', grid = True, legend = False, title = f'{file_out}', figsize = (19.20,10.80))
+df5.plot( grid = True, legend = False, figsize = (19.20,10.80))
+plt.xlabel('tempo [s]', fontsize=22)
+plt.ylabel('Bateria [%]', fontsize=22)
+plt.suptitle(f'{file_out}'+" gráfico", fontsize= 26)
 plt.savefig(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg', dpi = 600)
 plt.show()
 
 opt = input('Truncar gráfico a partir de um tempo?[s] ou [n]: ')
 if (opt == 'N' or 'n'): pass
-    # print('\n Encerrado')
-elif (opt == 'S'or 's'):
+
+elif (opt == 'S'or 's'):#drop rows based on user input
     zerar = int(input('Digite a partir de qual tempo em segundos os dados serão eliminados: '))
     df5 = df5.drop(range(zerar, int(df5.index[-1])+1))
-    print(df5)
-    df5.plot(xlabel= 'tempo[s]', ylabel = 'Bateria[%]', grid = True, legend = False, title = f'{file_out}', figsize = (19.20,10.80))
-    plt.savefig(f"C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg", dpi = 600)
-    #plt.show()
+    df5.plot( grid = True, legend = False, figsize = (19.20,10.80))
+    plt.xlabel('tempo [s]', fontsize=22)
+    plt.ylabel('Bateria [%]', fontsize=22)
+    plt.suptitle(f'{file_out}'+" gráfico", fontsize= 26)
+    plt.savefig(f'C:\\Users\\VNTTAMA\\Desktop\\Relatorios\\imagens\\{file_out}.jpg', dpi = 600)
+    plt.show()
 
 media = (round(df4["Tensão"].mean(), 5)) 
 minimo = (round(df4["Tensão"].min(), 5))
 maximo = (round(df4["Tensão"].max(), 5))
 
-min_index = df4.index[df4["Tensão"] == df4["Tensão"].min()]
-min_index_list = min_index.tolist()
-t_descarga = time.strftime('%H:%M:%S', time.gmtime(min_index_list[0]))
+min_index = df4.index[df4["Tensão"] == df4["Tensão"].min()]#calculete the min voltage 
+min_index_list = min_index.tolist()#save a list with index based on min voltage
+t_descarga = time.strftime('%H:%M:%S', time.gmtime(min_index_list[0]))#convert seconds to hh:mm:ss format
 
 header = input('É possível observar a carga pelo header? [s] ou [n]: ')
-
 t_header =[]
 t_conv_list = []
+battery = []
 if (header =='s' or header== 'S'):
     barras = int(input('Quantidade total de barras do header: '))
     barras2 = barras-1
-    for i in range(0, barras):
-        t_convert = (str(input(f'Tempo para atingir {barras2} barras: ')))   
+    for k in range(barras,0,-1):#user input on time to header show battey %
+        t_convert = (str(input(f'Tempo para atingir {barras2} barras: ')))#time expected input hh:mm:ss   
         t_header.append(t_convert)
-        h, m, s = t_convert.split(':')
+        h, m, s = t_convert.split(':')#convert hh:mm:ss format to seconds
         t_convert2 = int(h)*3600 + int(m)*60 + int(s)
-        t_conv_list.append(t_convert2)
+        t_conv_list.append(t_convert2)#pdf output will show hh:mm:ss and seconds format
+        battery.append(df5.iloc[t_convert2, 0])#locate battery% based on integer index
         barras2 = barras2-1    
-    #       
+    battery = [int(round(n, 0)) for n in battery ]# show battery % as integer rounded       
 else:
-    comment = str(input('Comente: '))    
-
+    comment = str(input('Comente: '))#if not show on header battery% user input a commnet about    
 
 saveAsPDF()
-#saveAstxt()
+
+print("-"*60)
+print(f"{file_out}.png foi salvo em Relatorios\imagens\n{file_out}.pdf foi salvo em Relatorios\pdf\n{file_out}.txt foi salvo em Relatorios"+'\\txt')
+print("-"*30+"FIM"+"-"*30)
+
+text = {'Tensao média': media, 'Tensão miníma': minimo, 'Tensão máxima': maximo, 'Tempo de carregamento': t_descarga}
+saveAstxt()
